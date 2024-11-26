c1b2477d30b74844bb25b7cc59fc8f795a98af21
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-11-25 19:00:47 -0600

    Using functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,28 @@
 
 using namespace std;
 
-int main(int argc, char **argv){
-    if(argc != 3)
-        return 0;
-    
-    DataBase db_2022(argv[1], 2022);
-    DataBase db_2023(argv[2], 2023);
-
-    //Unión de conjuntos:
+void set_union(DataBase &db_2022, DataBase &db_2023){
     DataBase set_union = db_2022 | db_2023;
     cout << "Set Union:" << endl;
     print(set_union);
     cout << endl;
+}
 
-    //Intersección de conjuntos:
+void set_intersect(DataBase &db_2022, DataBase &db_2023){
     DataBase intersection = db_2022 & db_2023;
     cout << "Set Intersection:" << endl;
     print(intersection);
     cout << endl;
+}
 
-    //Diferencias de conjuntos:
+void set_symm_diff(DataBase &db_2022, DataBase &db_2023){
+    DataBase symm_diff = db_2022 ^ db_2023;
+    cout << "Symmetric difference:" << endl;
+    print(symm_diff);
+    cout << endl;
+}
+
+void set_diff(DataBase &db_2022, DataBase &db_2023){
     //2022 - 2023
     DataBase former_not_latter = db_2022 - db_2023;
     cout << "2022 books except 2023:" << endl;
@@ -40,12 +42,26 @@ int main(int argc, char **argv){
     cout << "Genres' frequencies" << endl;
     latter_not_former.show_genres();
     cout << endl;
+}
+
+int main(int argc, char **argv){
+    if(argc != 3)
+        return 0;
+    
+    DataBase db_2022(argv[1], 2022);
+    DataBase db_2023(argv[2], 2023);
+
+    //Unión de conjuntos:
+    set_union(db_2022, db_2023);
+
+    //Intersección de conjuntos:
+    set_intersect(db_2022, db_2023);
+
+    //Diferencias de conjuntos:
+    set_diff(db_2022, db_2023);
 
     //Diferencia simétrica
-    DataBase symm_diff = db_2022 ^ db_2023;
-    cout << "Symmetric difference:" << endl;
-    print(symm_diff);
-    cout << endl;
+    set_symm_diff(db_2022, db_2023);
 
     return 0;
 }
